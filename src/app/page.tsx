b0a4336c5c12755612cb9e8dbe6ad07889ca93d8
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2023-05-08 17:23:43 +0300

    before adding mantine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+import { signIn } from "next-auth/react";
+
 export default function Home() {
-  return <h1>Homepage</h1>;
+  const signInWithGoogle = async () => {
+    try {
+      await signIn("google");
+    } catch (error) {
+      throw new Error("Something Went Wrong");
+    }
+  };
+  return (
+    <button
+      className="bg-dark-50 text-white rounded-md p-4"
+      onClick={signInWithGoogle}
+    >
+      Continue With Google
+    </button>
+  );
 }
